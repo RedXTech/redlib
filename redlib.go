fc3123e11c669f263e7f39d208de7c3e38e6cfdd
Gabe Dunn <gabe.dunn@shaw.ca>
2017-06-10 19:33:37 -0600

    added post

--- redlib.go
+++ redlib.go
@@ -7,6 +7,9 @@ import (
 	"os"
 	"strings"
 	"errors"
+	"net/url"
+	"bytes"
+	"unsafe"
 )
 
 func Readln() string {
@@ -27,4 +30,31 @@ func Get(url string) (string, error) {
 		return "", errors.New(err2.Error())
 	}
 	return string(bs), nil
+}
+
+func Post(postUrl string, data map[string]string) string {
+	hc := http.Client{}
+	routerURL := postUrl
+	form := url.Values{}
+
+	for k, v := range data {
+		form.Add(k, v)
+	}
+
+	req, err := http.NewRequest("POST", routerURL, strings.NewReader(form.Encode()))
+	if err != nil {
+	panic(err)
+	}
+	req.PostForm = form
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := hc.Do(req)
+	if err != nil {
+	panic(err)
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	b := buf.Bytes()
+	s := *(*string)(unsafe.Pointer(&b))
+	return s
 }
