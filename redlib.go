24b54becf9784c20f64d3ede45e23a61b24fb0d6
Gabe Dunn <gabe.dunn@shaw.ca>
2017-06-17 13:47:13 -0600

    added exists and string in slice

--- redlib.go
+++ redlib.go
@@ -2,15 +2,15 @@ package redlib
 
 import (
 	"bufio"
+	"bytes"
+	"errors"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
-	"errors"
-	"net/url"
-	"bytes"
 	"unsafe"
-	"log"
 )
 
 func Readln() string {
@@ -44,20 +44,40 @@ func Post(postUrl string, data map[string]string) string {
 
 	req, err := http.NewRequest("POST", routerURL, strings.NewReader(form.Encode()))
 	if err != nil {
-	panic(err)
+		panic(err)
 	}
 	req.PostForm = form
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0")
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	resp, err := hc.Do(req)
 	if err != nil {
-	panic(err)
+		panic(err)
 	}
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(resp.Body)
 	b := buf.Bytes()
 	s := *(*string)(unsafe.Pointer(&b))
 	return s
+}
+
+func Exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
+
+func StringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
 
 func HandleErr(err error) bool {
